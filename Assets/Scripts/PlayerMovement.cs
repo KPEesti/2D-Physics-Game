c86633a94c90304d41731adffcd902ec87588ec4
qwqw ffe <misha.torno@gmail.com>
2021-04-02 13:15:04 +0500

    дописал скрипт передвижения персонажа(для прыжка нужно выставлять, что такое groung, силу прыжка, добавлять позицию ног) проверяю, что такое земля с помощью overlapcircle, там есть радиус он по дефолту равен 0.2f

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,31 @@ using static Lever;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
-    
+    [SerializeField] private float jumpForce;
+    [SerializeField] private Transform leg;
+    [SerializeField] private LayerMask whatIsGround;
+
     private bool facingRight;
     private float horizontalMovement;
     private Transform player;
+    private Rigidbody2D rb;
+    private bool isGrounded;
 
     private void Start()
     {
         facingRight = true;
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         horizontalMovement = Input.GetAxisRaw("Horizontal");
+
+        isGrounded = Physics2D.OverlapCircle(leg.position, 0.2f, whatIsGround);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            rb.velocity = Vector2.up * jumpForce;
 
         if (horizontalMovement < 0 && facingRight || horizontalMovement > 0 && !facingRight)
             Flip();
