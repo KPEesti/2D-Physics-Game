4e9655526732dbad12dc771002f86af1d41e4d79
qwqw ffe <misha.torno@gmail.com>
2021-06-07 14:43:20 +0500

    убрал баг с вызовом ивента

--- Assets/Scripts/MainCameraScripts/CameraTrigger.cs
+++ Assets/Scripts/MainCameraScripts/CameraTrigger.cs
@@ -7,7 +7,15 @@ public class CameraTrigger : MonoBehaviour
     public UnityEvent EnterTriggerEvent;
     public UnityEvent ExitTriggerEvent;
 
-    private void OnTriggerEnter2D(Collider2D other) => EnterTriggerEvent?.Invoke();
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (tag is "CameraTrigger")
+            EnterTriggerEvent?.Invoke();
+    }
 
-    private void OnTriggerExit2D(Collider2D other) => ExitTriggerEvent?.Invoke();
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (tag is "CameraTrigger")
+            ExitTriggerEvent?.Invoke();
+    }
 }
