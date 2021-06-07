df7fc6d44e42525cfb89fbe0c0a5243e57f8e458
SeriousDim <dlykov2001@yandex.ru>
2021-06-07 19:23:15 +0500

    new paths

---
+++ Assets/Scripts/Cave/CrystalScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalScript : MonoBehaviour
+{
+    public GameObject crystalButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.name == "Player")
+            crystalButtons.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.name == "Player")
+            crystalButtons.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Cave/NewLaserSource.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewLaserSource : MonoBehaviour
+{
+    public int numberReflectionMax = 5;
+
+    [HideInInspector]
+    public GameObject lastRaycastedObject;
+
+    private LineRenderer lineRenderer;
+    private int laserDistance = 100;
+    private Vector3 pos = new Vector3();
+    private Vector3 directLaser = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawLaser();
+    }
+
+    private void DrawLaser()
+    {
+        int countLaser = 1;
+
+        pos = transform.position;
+        directLaser = transform.right;
+        lineRenderer.positionCount = countLaser;
+        lineRenderer.SetPosition(0, pos);
+
+        while (true)
+        {
+            var hit = Physics2D.Raycast(pos, directLaser, laserDistance, LayerMask.GetMask("Default", "world"));
+            if (hit)
+            {
+                countLaser++;
+                lineRenderer.positionCount = countLaser;
+                directLaser = Vector3.Reflect(directLaser, hit.normal);
+                // передвигаем новую стартовую точку чуть вперед по отраженному направлению, так как она может оказаться
+                // внутри объекта из-за погрешности вычислений
+                pos = (Vector2)directLaser.normalized + hit.point;
+                lineRenderer.SetPosition(countLaser - 1, hit.point); // при этом рисуем точку касания без изменений
+
+                lastRaycastedObject = hit.collider.gameObject;
+                Debug.Log("Last raycasted obj: "+lastRaycastedObject.name);
+            }
+            else
+            {
+                countLaser++;
+                lineRenderer.positionCount = countLaser;
+                lineRenderer.SetPosition(countLaser - 1, pos + (directLaser.normalized * laserDistance));
+                break;
+            }
+
+            if (countLaser > numberReflectionMax || !hit.collider.tag.Equals("Mirror"))
+                break;
+        }
+    }
+}
