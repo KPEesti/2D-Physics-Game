96ba0da064043ff8fe22bf7f68a36ef15043a161
AndrewShvets <shvetsac@gmail.com>
2021-06-15 17:57:20 +0500

    меню готово

--- Assets/Scripts/_Menu.cs
+++ Assets/Scripts/_Menu.cs
@@ -2,26 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class _Menu : MonoBehaviour
 {
+    public Button level2;
     private int levelComplete;
-    // Start is called before the first frame update
+    private int sceneCount;
+    
+
     void Start()
     {
         levelComplete = PlayerPrefs.GetInt("LevelComplete");
+        sceneCount = SceneManager.sceneCountInBuildSettings;
+        
     }
 
     public void LoadNextLevel()
     {
-        var sceneCount = SceneManager.sceneCountInBuildSettings;
         var nextLevel = levelComplete + 1 < sceneCount ? levelComplete + 1 : 0;
         SceneManager.LoadScene(nextLevel);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GoToLevel(int level)
     {
-        
+        SceneManager.LoadScene(level);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
